598a31bf9ca50857ce4d4d9575e30370c7834ef7
Rananjay Talwar <91244685+rananjay3516@users.noreply.github.com>
2022-08-02 06:44:16 +0530

    Update App.js

--- App.js
+++ App.js
@@ -3,17 +3,12 @@ import { StatusBar } from "expo-status-bar";
 //navigation setup
 import Navigator from "./src/components/navigator";
 
-// redux store
-import { store } from "./src/store/store";
-import { Provider } from "react-redux";
-
 export default function App() {
   return (
     <>
       <StatusBar style="auto" />
-      <Provider store={store}>
-        <Navigator />
-      </Provider>
+
+      <Navigator />
     </>
   );
 }
